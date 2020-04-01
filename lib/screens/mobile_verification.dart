aad9e01ceedf299f752407ef2984d91b482400fa
hirishegan <khirishegan@gmail.com>
2020-04-02 01:52:35 +0530

    alignment overflow error resolved for mobile_verification and profile creation

--- lib/screens/mobile_verification.dart
+++ lib/screens/mobile_verification.dart
@@ -24,61 +24,68 @@ class _MobileVerificationPageState extends State<MobileVerificationPage> {
           ),
         ),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 5,
-                child: Center(
-                  child: Image.asset('assets/img/phone.png'),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              Expanded(
-                  flex: 4,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Text(
-                        'Enter your mobile number to verify',
+                Center(
+                  child: Image.asset(
+                    'assets/img/phone.png',
+                  ),
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      'Enter your mobile number to verify',
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 45.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 20),
                         textAlign: TextAlign.center,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 45.0),
-                        child: TextField(
-                          style: TextStyle(fontSize: 20),
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            hintText: "07X XXX XXXX",
-                            // labelText: 'Phone Number'
-                          ),
-                          onChanged: (value) {
-                            phoneNo = value;
-                            print(phoneNo);
-                          },
+                        decoration: InputDecoration(
+                          hintText: "07X XXX XXXX",
+                          // labelText: 'Phone Number'
                         ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      BigButton(
-                        text: 'Proceed',
-                        onPressed: () {
-                          print('pressed');
-                          Navigator.of(context)
-                              .pushReplacement(MaterialPageRoute(
-                                  builder: (context) => SuccessRegistrationPage(
-                                        phoneNo: phoneNo,
-                                      )));
+                        onChanged: (value) {
+                          phoneNo = value;
+                          print(phoneNo);
                         },
-                      )
-                    ],
-                  ))
-            ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    BigButton(
+                      text: 'Proceed',
+                      onPressed: () {
+                        print('pressed');
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => SuccessRegistrationPage(
+                                  phoneNo: phoneNo,
+                                )));
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ));
   }
--- lib/screens/profile_creation.dart
+++ lib/screens/profile_creation.dart
@@ -26,67 +26,68 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
         ),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 5,
-                child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
+                ),
+                Center(
                   child: Image.asset('assets/img/profile.png'),
                 ),
-              ),
-              Expanded(
-                  flex: 4,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 45.0),
-                        child: TextField(
-                          style: TextStyle(fontSize: 20),
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                              hintText: "Eg: Hirishegan", labelText: 'Name'),
-                          onChanged: (value) {
-                            name = value;
-                            print(name);
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 45.0),
-                        child: TextField(
-                          style: TextStyle(fontSize: 20),
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                              hintText: "Eg: Alergic",
-                              labelText: 'More Details'),
-                          onChanged: (value) {
-                            otherDetails = value;
-                            print(otherDetails);
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      BigButton(
-                        text: 'Save',
-                        onPressed: () {
-                          print('pressed');
-                          Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                  builder: (context) => BottomNavigation()));
+                SizedBox(
+                  height: 100,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 45.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                            hintText: "Eg: Hirishegan", labelText: 'Name'),
+                        onChanged: (value) {
+                          name = value;
+                          print(name);
                         },
-                      )
-                    ],
-                  ))
-            ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 45.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                            hintText: "Eg: Alergic", labelText: 'More Details'),
+                        onChanged: (value) {
+                          otherDetails = value;
+                          print(otherDetails);
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    BigButton(
+                      text: 'Save',
+                      onPressed: () {
+                        print('pressed');
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => BottomNavigation()));
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ));
   }
