e9b8f9e2c471d753a7563f972f61f1ec8384155d
hirishegan <khirishegan@gmail.com>
2020-04-06 01:08:35 +0530

    firebase auth connected with firebase

--- lib/screens/mobile_verification.dart
+++ lib/screens/mobile_verification.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:user/components/big_button.dart';
 import 'package:user/screens/success_registration.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
 
 class MobileVerificationPage extends StatefulWidget {
   const MobileVerificationPage({Key key}) : super(key: key);
@@ -11,6 +13,104 @@ class MobileVerificationPage extends StatefulWidget {
 
 class _MobileVerificationPageState extends State<MobileVerificationPage> {
   String phoneNo;
+  String smsCode;
+  AuthCredential _phoneAuthCredential;
+  FirebaseUser _firebaseUser;
+  var _verificationId;
+
+  Future<void> _logout() async {
+    /// Method to Logout the `FirebaseUser` (`_firebaseUser`)
+    try {
+      // signout code
+      await FirebaseAuth.instance.signOut();
+      _firebaseUser = null;
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> _login() async {
+    /// This method is used to login the user
+    /// `AuthCredential`(`_phoneAuthCredential`) is needed for the signIn method
+    /// After the signIn method from `AuthResult` we can get `FirebaserUser`(`_firebaseUser`)
+    try {
+      await FirebaseAuth.instance
+          .signInWithCredential(this._phoneAuthCredential)
+          .then((AuthResult authRes) {
+        _firebaseUser = authRes.user;
+        print(_firebaseUser.toString());
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void _submitOTP() {
+    /// when used different phoneNumber other than the current (running) device
+    /// we need to use OTP to get `phoneAuthCredential` which is inturn used to signIn/login
+    this._phoneAuthCredential = PhoneAuthProvider.getCredential(
+        verificationId: this._verificationId, smsCode: this.smsCode);
+
+    _login();
+  }
+
+  Future<bool> smsCodeDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: Text('Enter sms Code'),
+            content: TextField(
+              onChanged: (value) {
+                this.smsCode = value;
+                print(this.smsCode);
+              },
+            ),
+            contentPadding: EdgeInsets.all(10.0),
+            actions: <Widget>[
+              new FlatButton(
+                child: Text('Done'),
+                onPressed: () {
+                  _submitOTP();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  Future<void> _submitPhoneNumber() async {
+    void verificationCompleted(AuthCredential phoneAuthCredential) {
+      print('verificationCompleted');
+
+      this._phoneAuthCredential = phoneAuthCredential;
+      print(phoneAuthCredential);
+    }
+
+    void verificationFailed(AuthException error) {
+      print(error);
+    }
+
+    void codeSent(String verificationId, [int code]) {
+      this._verificationId = verificationId;
+      print('codeSent');
+      smsCodeDialog(context);
+    }
+
+    void codeAutoRetrievalTimeout(String verificationId) {
+      print('codeAutoRetrievalTimeout');
+    }
+
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      phoneNumber: phoneNo,
+      timeout: Duration(milliseconds: 10000),
+      verificationCompleted: verificationCompleted,
+      verificationFailed: verificationFailed,
+      codeSent: codeSent,
+      codeAutoRetrievalTimeout: codeAutoRetrievalTimeout,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,10 +173,11 @@ class _MobileVerificationPageState extends State<MobileVerificationPage> {
                       text: 'Proceed',
                       onPressed: () {
                         print('pressed');
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => SuccessRegistrationPage(
-                                  phoneNo: phoneNo,
-                                )));
+                        // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        //     builder: (context) => SuccessRegistrationPage(
+                        //           phoneNo: phoneNo,
+                        //         )));
+                        _submitPhoneNumber();
                       },
                     ),
                     SizedBox(
--- lib/screens/order_page.dart
+++ lib/screens/order_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:user/components/order_card.dart';
 import 'package:user/screens/order_confirmation.dart';
 
