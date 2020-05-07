6e82ce59f5d49922d72a0570bd3379a035afbb2a
hirishegan <khirishegan@gmail.com>
2020-05-08 02:22:48 +0530

    final commit

--- lib/screens/profile_creation.dart
+++ lib/screens/profile_creation.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:user/components/big_button.dart';
+import 'package:user/constants.dart';
 import 'bottom_navigation.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -14,9 +15,13 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   String name;
+  String address;
   String otherDetails;
   FirebaseUser _firebaseUser;
   final db = Firestore.instance;
+  int spice = 3;
+  int suger = 3;
+  int oil = 3;
 
   void getCurrentUser() async {
     _firebaseUser = await FirebaseAuth.instance.currentUser();
@@ -84,11 +89,12 @@ class _ProfilePageState extends State<ProfilePage> {
                       padding: const EdgeInsets.symmetric(horizontal: 45.0),
                       child: TextField(
                         style: TextStyle(fontSize: 20),
-                        textAlign: TextAlign.center,
                         decoration: InputDecoration(
-                            hintText: "Eg: Hirishegan", labelText: 'Name'),
+                            hintText: "ex: Hirishegan Karuneswaran",
+                            labelText: 'Name'),
                         onChanged: (value) {
                           name = value;
+                          setState(() {});
                           print(name);
                         },
                       ),
@@ -100,26 +106,138 @@ class _ProfilePageState extends State<ProfilePage> {
                       padding: const EdgeInsets.symmetric(horizontal: 45.0),
                       child: TextField(
                         style: TextStyle(fontSize: 20),
-                        textAlign: TextAlign.center,
                         decoration: InputDecoration(
-                            hintText: "Eg: Alergic", labelText: 'More Details'),
+                            hintText: "ex: Door No, Street, City",
+                            labelText: 'Address'),
                         onChanged: (value) {
-                          otherDetails = value;
-                          print(otherDetails);
+                          address = value;
+                          setState(() {});
+                          print(address);
                         },
                       ),
                     ),
                     SizedBox(
                       height: 40,
                     ),
-                    BigButton(
-                      text: 'Save',
-                      onPressed: () {
-                        print('pressed');
-                        registerUser(); //register user to the firestore
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => BottomNavigation()));
-                      },
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 45.0),
+                      // child: TextField(
+                      //   style: TextStyle(fontSize: 20),
+                      //   textAlign: TextAlign.center,
+                      //   decoration: InputDecoration(
+                      //       hintText: "Eg: Alergic", labelText: 'More Details'),
+                      //   onChanged: (value) {
+                      //     otherDetails = value;
+                      //     print(otherDetails);
+                      //   },
+                      // ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Select your preference',
+                            style: TextStyle(color: Colors.grey.shade700),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text('Spice level'),
+                          SliderTheme(
+                            data: SliderTheme.of(context).copyWith(
+                              activeTrackColor: Colors.green,
+                              inactiveTrackColor: Colors.grey,
+                              thumbColor: kBigButtonColor,
+                              overlayColor: kBigButtonColor,
+                              thumbShape: RoundSliderThumbShape(
+                                  enabledThumbRadius: 10.0),
+                              overlayShape:
+                                  RoundSliderOverlayShape(overlayRadius: 20.0),
+                            ),
+                            child: Slider(
+                              value: spice.toDouble(),
+                              min: 1,
+                              max: 6,
+                              onChanged: (double value) {
+                                setState(() {
+                                  spice = value.round();
+                                });
+                              },
+                            ),
+                          ),
+                          Text('Oil level'),
+                          SliderTheme(
+                            data: SliderTheme.of(context).copyWith(
+                              activeTrackColor: Colors.green,
+                              inactiveTrackColor: Colors.grey,
+                              thumbColor: kBigButtonColor,
+                              overlayColor: kBigButtonColor,
+                              thumbShape: RoundSliderThumbShape(
+                                  enabledThumbRadius: 10.0),
+                              overlayShape:
+                                  RoundSliderOverlayShape(overlayRadius: 20.0),
+                            ),
+                            child: Slider(
+                              value: oil.toDouble(),
+                              min: 1,
+                              max: 6,
+                              onChanged: (double value) {
+                                setState(() {
+                                  oil = value.round();
+                                });
+                              },
+                            ),
+                          ),
+                          Text('Suger level'),
+                          SliderTheme(
+                            data: SliderTheme.of(context).copyWith(
+                              activeTrackColor: Colors.green,
+                              inactiveTrackColor: Colors.grey,
+                              thumbColor: kBigButtonColor,
+                              overlayColor: kBigButtonColor,
+                              thumbShape: RoundSliderThumbShape(
+                                  enabledThumbRadius: 10.0),
+                              overlayShape:
+                                  RoundSliderOverlayShape(overlayRadius: 20.0),
+                            ),
+                            child: Slider(
+                              value: suger.toDouble(),
+                              min: 1,
+                              max: 6,
+                              onChanged: (double value) {
+                                setState(() {
+                                  suger = value.round();
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: BigButton(
+                        bgColor: name != null &&
+                                address != null &&
+                                name != '' &&
+                                address != ''
+                            ? kBigButtonColor
+                            : Colors.grey.shade300,
+                        text: 'Save',
+                        onPressed: () {
+                          print('pressed');
+                          registerUser(); //register user to the firestore
+                          if (name != null &&
+                              address != null &&
+                              name != '' &&
+                              address != '')
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => BottomNavigation()));
+                        },
+                      ),
                     )
                   ],
                 )
